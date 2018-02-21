f2c4e0811147f1d2199fe5257a995df5e3409e49
AlexCaswell <alex.cswll@gmail.com>
2018-02-20 18:12:32 -0600

    Added files

---
+++ js/server_requests.js
@@ -0,0 +1,88 @@
+/******************************************
+
+server request and settings functions 
+
+*******************************************/
+
+
+const pathToPredefinedResponses = "predefined_json_responses/";
+
+
+/***********************************
+SERVER REQUEST FUNCTIONS
+************************************/
+
+// return the response (in JS object) of the "getTeam" server request.
+function getTeamModel() {
+
+}
+
+
+// returns JS object containing the contents of the "ui" object within the "Team model"
+function getUiModel() {
+
+}
+
+
+// returns active_event_name string
+function getActiveEventName() {
+
+}
+
+
+// returns array of all checkouts filtered by options
+function getCheckouts(showPitCheckouts, showCompletedCheckouts, showCheckedOut) {
+
+}
+
+
+// uploads array of checkout model objects
+function pushCheckouts(checkouts) {
+
+}
+
+
+
+/************************************
+SETTINGS FUCNTIONS
+************************************/
+
+// returns team code from localStorage
+function getTeamCode() {
+
+}
+
+// sets team code in localStorage
+function setTeamCode() {
+
+}
+
+
+// username
+function getUsername() {
+
+}
+
+function setUsername() {
+
+}
+
+
+// server ip
+function getServerIp() {
+
+}
+
+function setServerIp() {
+
+}
+
+
+// auto-chekout mode
+function getAutoCheckoutMode() {
+
+}
+
+function setAutoCheckoutMode() {
+
+}
