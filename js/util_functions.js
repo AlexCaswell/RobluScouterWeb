1e7bb69bcba9b78b532115d319c5d34173efacc5
AlexCaswell <alex.cswll@gmail.com>
2018-02-21 21:32:09 -0600

    changed file structure

--- js/util_functions.js
+++ js/util_functions.js
@@ -9,15 +9,13 @@ Contains utility functions used throughout app
 
 //translates autoCheckoutMode setting from number to title
 function getAutoCheckoutModeString(ac_mode) {
-	switch(ac_mode) {
-		case 0: return null;
-		case 1: return "Red Device 1";
-		case 2: return "Red Device 2";
-		case 3: return "Red Device 3";
-		case 4: return "Blue Device 1";
-		case 5: return "Blue Device 2";
-		case 6: return "Blue device 3";
-		case 7: return "Pit";
-		default: return null;
-	}
-}
+	if (ac_mode == 0) { return null; }
+	else if (ac_mode == 1) { return "Red Device 1"; }
+	else if (ac_mode == 2) { return "Red Device 2"; }
+	else if (ac_mode == 3) { return "Red Device 3"; }
+	else if (ac_mode == 4) { return "Blue Device 1"; }
+	else if (ac_mode == 5) { return "Blue Device 2"; }
+	else if (ac_mode == 6) { return "Blue Device 3"; }
+	else if (ac_mode == 7) { return "Pit"; }
+	else { return null; }
+} 
