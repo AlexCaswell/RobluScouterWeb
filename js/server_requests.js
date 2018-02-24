7e99a2535a1b21bea6bd5bfd41a9174831202c15
AlexCaswell <alex.cswll@gmail.com>
2018-02-24 01:20:45 -0600

    added settings functionality

--- js/server_requests.js
+++ js/server_requests.js
@@ -1,29 +1,27 @@
 /******************************************
 
-server request and settings functions (depends on Jquery)
+server request functions
+(uses Jquery)
 
 *******************************************/
 
 
-const pathToPredefinedResponses = "predefined_json_responses/";
-
 
 /***********************************
 SERVER REQUEST FUNCTIONS
+
+NOTE:
+Each of these functions assume that the server inforamtion has already stored in localStorage items
 ************************************/
 
 // return the response (in JS object) of the "getTeam" server request.
 function getTeamModel(callback) {
-	getServerIp( function( ip ) {
-		getTeamCode( function( code ) {
-			$.ajax({
-				type: 'GET',
-				url: "http://" + ip + "/teams/getTeam",
-				data: { code: code }
-			}).done( function ( data ) {
-				callback(data.data);
-			});
-		})
+	$.ajax({
+		type: 'GET',
+		url: "http://" + localStorage.getItem("serverIp") + "/teams/getTeam",
+		data: { code: localStorage.getItem("teamCode") }
+	}).done( function ( data ) {
+		callback(data.data);
 	});
 }
 
@@ -31,92 +29,88 @@ function getTeamModel(callback) {
 
 // returns array of all checkouts filtered by options
 function getCheckouts(showPitCheckouts, showCompletedCheckouts, showCheckedOut, deviceRole, callback) {
-	getServerIp( function( ip ) {
-		getTeamCode( function( code ) {
-			$.ajax({
-				type: 'GET',
-				url: "http://" + ip + "/checkouts/pullCheckouts",
-				data: { code: code, time: Math.round((new Date()).getTime() / 1000) }
-			}).done( function ( data ) {
+	$.ajax({
+		type: 'GET',
+		url: "http://" + localStorage.getItem("serverIp") + "/checkouts/pullCheckouts",
+		data: { code: localStorage.getItem("teamCode"), time: Math.round((new Date()).getTime() / 1000) }
+	}).done( function ( data ) {
 
-				// sort checkouts based on "checkbox variables"
-				var sorted_checkouts_c = [];
-				var index = 0;
+		// sort checkouts based on "checkbox variables"
+		var sorted_checkouts_c = [];
+		var index = 0;
 
-			    for (var i = 0; i < data.data.length; i++) {
-			    	var checkout = JSON.parse(data.data[i].content);
-			    	// Show pit checkouts
-			    	if(showPitCheckouts && checkout.team.tabs[0].matchType == "PIT") {
-			    		sorted_checkouts_c[index] = checkout;
-			    		index++;
-			    	} else if(checkout.team.tabs[0].matchType == "PIT") { continue; }
-			    	// Show comleted checkouts
-			    	if(showCompletedCheckouts && checkout.status == 2) {
-			    		sorted_checkouts_c[index] = checkout;
-			    		index++;
-			    	} else if (checkout.status == 2) { continue; }
-			    	// Show checked out
-			    	if(showCheckedOut && checkout.status == 1) {
-			    		sorted_checkouts_c[index] = checkout;
-			    		index++;
-			    	}
-			    }
-			    // sort remaining checkouts by device role
-			    var sorted_checkouts_cd = [];
-			    index = 0;
+	    for (var i = 0; i < data.data.length; i++) {
+	    	var checkout = JSON.parse(data.data[i].content);
+	    	// Show pit checkouts
+	    	if(showPitCheckouts && checkout.team.tabs[0].matchType == "PIT") {
+	    		sorted_checkouts_c[index] = checkout;
+	    		index++;
+	    	} else if(checkout.team.tabs[0].matchType == "PIT") { continue; }
+	    	// Show comleted checkouts
+	    	if(showCompletedCheckouts && data.data[i].status == 3) {
+	    		sorted_checkouts_c[index] = checkout;
+	    		index++;
+	    	} else if (data.data[i].status == 3) { continue; }
+	    	// Show checked out
+	    	if(showCheckedOut && data.data[i].status == 2) {
+	    		sorted_checkouts_c[index] = checkout;
+	    		index++;
+	    	}
+	    }
+	    // sort remaining checkouts by device role
+	    var sorted_checkouts_cd = [];
+	    index = 0;
 
-			    if(deviceRole != 0) {
-			    	for(var i = 0; i < sorted_checkouts_c.length; i++) {
-			    		var checkout = sorted_checkouts_c[i];
+	    if(deviceRole != 0) {
+	    	for(var i = 0; i < sorted_checkouts_c.length; i++) {
+	    		var checkout = sorted_checkouts_c[i];
 
-			    		// Pit role
-				    	if(checkout.team.tabs[0].matchType == "PIT" && deviceRole == 7) {
-				    		sorted_checkouts_cd[index] = checkout;
-				    		index++;
-				    	} else if (deviceRole == 7) { continue; }
-				    	// Blue 3 role
-				    	if(checkout.team.tabs[0].alliancePosition == 6 && deviceRole == 6) {
-				    		sorted_checkouts_cd[index] = checkout;
-				    		index++;
-				    	} else if (deviceRole == 6) { continue; }
-				    	// Blue 2 role
-				    	if(checkout.team.tabs[0].alliancePosition == 5 && deviceRole == 5) {
-				    		sorted_checkouts_cd[index] = checkout;
-				    		index++;
-				    	} else if (deviceRole == 6) { continue; }
-				    	// Blue 1 role
-				    	if(checkout.team.tabs[0].alliancePosition == 4 && deviceRole == 4) {
-				    		sorted_checkouts_cd[index] = checkout;
-				    		index++;
-				    	} else if (deviceRole == 6) { continue; }
-				    	// Red 3 role
-				    	if(checkout.team.tabs[0].alliancePosition == 3 && deviceRole == 3) {
-				    		sorted_checkouts_cd[index] = checkout;
-				    		index++;
-				    	} else if (deviceRole == 6) { continue; }
-				    	// Red 2 role
-				    	if(checkout.team.tabs[0].alliancePosition == 2 && deviceRole == 2) {
-				    		sorted_checkouts_cd[index] = checkout;
-				    		index++;
-				    	} else if (deviceRole == 6) { continue; }
-				    	// Red 1 role
-				    	if(checkout.team.tabs[0].alliancePosition == 1 && deviceRole == 1) {
-				    		sorted_checkouts_cd[index] = checkout;
-				    		index++;
-				    	} else if (deviceRole == 7) { continue; }
-			   		}
+	    		// Pit role
+		    	if(checkout.team.tabs[0].matchType == "PIT" && deviceRole == 7) {
+		    		sorted_checkouts_cd[index] = checkout;
+		    		index++;
+		    	} else if (deviceRole == 7) { continue; }
+		    	// Blue 3 role
+		    	if(checkout.team.tabs[0].alliancePosition == 6 && deviceRole == 6) {
+		    		sorted_checkouts_cd[index] = checkout;
+		    		index++;
+		    	} else if (deviceRole == 6) { continue; }
+		    	// Blue 2 role
+		    	if(checkout.team.tabs[0].alliancePosition == 5 && deviceRole == 5) {
+		    		sorted_checkouts_cd[index] = checkout;
+		    		index++;
+		    	} else if (deviceRole == 5) { continue; }
+		    	// Blue 1 role
+		    	if(checkout.team.tabs[0].alliancePosition == 4 && deviceRole == 4) {
+		    		sorted_checkouts_cd[index] = checkout;
+		    		index++;
+		    	} else if (deviceRole == 4) { continue; }
+		    	// Red 3 role
+		    	if(checkout.team.tabs[0].alliancePosition == 3 && deviceRole == 3) {
+		    		sorted_checkouts_cd[index] = checkout;
+		    		index++;
+		    	} else if (deviceRole == 3) { continue; }
+		    	// Red 2 role
+		    	if(checkout.team.tabs[0].alliancePosition == 2 && deviceRole == 2) {
+		    		sorted_checkouts_cd[index] = checkout;
+		    		index++;
+		    	} else if (deviceRole == 2) { continue; }
+		    	// Red 1 role
+		    	if(checkout.team.tabs[0].alliancePosition == 1 && deviceRole == 1) {
+		    		sorted_checkouts_cd[index] = checkout;
+		    		index++;
+		    	} else if (deviceRole == 1) { continue; }
+	   		}
 
-		   		//return sorted checkouts
-		   		callback(sorted_checkouts_cd);
+   		//return sorted checkouts
+   		callback(sorted_checkouts_cd);
 
-			    }
-			    else {
-			    	// No role
-			    	callback(sorted_checkouts_c);
-			    }
+	    }
+	    else {
+	    	// No role
+	    	callback(sorted_checkouts_c);
+	    }
 
-			});
-		})
 	});
 }
 
@@ -127,53 +121,3 @@ function pushCheckouts(checkouts) {
 }
 
 
-
-/************************************
-SETTINGS FUCNTIONS
-************************************/
-
-// returns team code from localStorage
-function getTeamCode(callback) {
-	$.getJSON("../default_settings.json", function(json) {
-		callback(json.teamCode);
-	});
-}
-
-// sets team code in localStorage
-function setTeamCode() {
-
-}
-
-
-// username
-function getUsername() {
-
-}
-
-function setUsername() {
-
-}
-
-
-// server ip
-function getServerIp(callback) {
-	$.getJSON("../default_settings.json", function(json) {
-		callback(json.serverIp);
-	});
-}
-
-function setServerIp() {
-
-}
-
-
-// auto-chekout mode
-function getAutoCheckoutMode(callback) {
-	$.getJSON("../default_settings.json", function(json) {
-		callback(json.autoCheckoutMode);
-	});
-}
-
-function setAutoCheckoutMode() {
-
-}
--- js/util_functions.js
+++ js/util_functions.js
@@ -18,4 +18,15 @@ function getAutoCheckoutModeString(ac_mode) {
 	else if (ac_mode == 6) { return "Blue Device 3"; }
 	else if (ac_mode == 7) { return "Pit"; }
 	else { return null; }
-} 
+}
+
+
+//Checks if settings are set in localStorage. If they are not, set them to the defaults.
+function loadDefaultsIfNeeded() {
+	$.getJSON("../default_settings.json", function(json) {
+		if (localStorage.getItem("username") === "undefined") { localStorage.setItem("username", json.username); }
+		if (localStorage.getItem("serverIp") === "undefined") { localStorage.setItem("serverIp", json.serverIp); }
+		if (localStorage.getItem("teamCode") === "undefined") { localStorage.setItem("teamCode", json.teamCode); }
+		if (localStorage.getItem("autoCheckoutMode") === "undefined") { localStorage.setItem("autoCheckoutMode", json.autoCheckoutMode); }
+	});
+}
