23386e9e0ae82cd1e32b6675ba64c7728d879d1c
AlexCaswell <alex.cswll@gmail.com>
2018-02-24 19:38:29 -0600

    added edit_data page

--- cards/get_checkout.php
+++ cards/get_checkout.php
@@ -6,13 +6,22 @@ $type = $_GET['type'];
 $available = $_GET['available'];
 $card_color = $_GET['card_color'];
 $onclick = $_GET['onclick'];
+$checkout = $_GET['checkout'];
+$id = $_GET['id'];
+$match_order = $_GET['match_order']
 
 ?>
 
-<div onclick="<?php echo $onclick; ?>" class="card blue-grey darken-1" style="background: <?php echo $card_color; ?> !important;">
+<div id="<?php echo $id; ?>" checkout='<?php echo $checkout; ?>' onclick="<?php echo $onclick; ?>" class="card blue-grey darken-1" style="background: <?php echo $card_color; ?> !important;">
 	<div class="card-content white-text">
 		<span class="card-title"><?php echo $team; ?></span>
-		<p><?php echo $type; ?></p>
+		<p><?php
+			if($type == "PIT") {
+				echo $type;
+			}else {
+				echo $type." ".$match_order;
+			}
+		?></p>
 		<p><?php echo $available; ?></p>
 	</div>
 </div>
